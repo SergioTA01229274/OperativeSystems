4717ebc6fe931289ebb944bdc8ea3922bed00fcc
Sergio Ivan Tostado Nieto <A01229274@itesm.mx>
2019-09-08 01:04:26 +0000

    Tarea Llamadas a sistema

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -132,6 +132,8 @@ static int (*syscalls[])(void) = {
 [SYS_reboot] sys_reboot
 };
 
+const char *Syscalls[23] = {"fork", "exit", "wait", "pipe", "read", "kill", "exec", "fstat", "chdir", "dup", "getpid", "sbrk", "sleep", "uptime", "open", "write", "mknod", "unlink", "link", "mkdir", "close", "shutdown", "reboot"};
+
 void
 syscall(void)
 {
@@ -139,11 +141,14 @@ syscall(void)
   struct proc *curproc = myproc();
 
   num = curproc->tf->eax;
+  cprintf("%s -> %d ", Syscalls[num], num + 1);
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
     curproc->tf->eax = -1;
   }
+
 }
